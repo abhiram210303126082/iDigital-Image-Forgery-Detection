1b54dde66acff5a5861a6348ed163d7c7d583e42
abhiram <124145409+abhiram210303126082@users.noreply.github.com>
2025-01-20 15:17:42 +0530

    Add files via upload

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,23 +1,7 @@
-# This file is part of h5py, a Python interface to the HDF5 library.
-#
-# http://www.h5py.org
-#
-# Copyright 2008-2013 Andrew Collette and contributors
-#
-# License:  Standard 3-clause BSD; see "license.txt" for full license terms
-#           and contributor agreement.
+"""
+Tests for the array API namespace.
 
-
-def run_tests(args=''):
-    try:
-        from pytest import main
-    except ImportError:
-        print("Tests require pytest, pytest not installed")
-        return 1
-    else:
-        from shlex import split
-        from subprocess import call
-        from sys import executable
-        cli = [executable, "-m", "pytest", "--pyargs", "h5py"]
-        cli.extend(split(args))
-        return call(cli)
+Note, full compliance with the array API can be tested with the official array API test
+suite https://github.com/data-apis/array-api-tests. This test suite primarily
+focuses on those things that are not tested by the official test suite.
+"""
